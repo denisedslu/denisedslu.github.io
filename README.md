37fe6a200dbd7f4ce7ef53c86000b6131c60ae55
Denise Lu <denise.ds.lu@gmail.com>
2014-09-29 00:30:13 -0400

    site rehaul v3

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 denisedslu.github.io
 ====================
-page for deniselu.com
+page for [deniselu.com](http://deniselu.com/)
+
+shouts out to:
+
++ Mozilla/Carrois for [Fira](http://www.carrois.com/fira-3-1/)
++ Henrik Joreteg for [ICanHaz](http://icanhazjs.com/)
---
+++ js/script.js
@@ -0,0 +1,83 @@
+function init() {
+
+        ich.grabTemplates();
+        getData();
+        eventListeners();
+
+    }
+
+
+    function getData() {
+      $.each(writing, function(k,v) {
+          
+          var writing_block = ich.writing_block(v);
+          var writing_also_block = ich.writing_also_block(v);
+
+          if (v.also !== '') {
+              $('div.row#writing').find('ul[data-pub="' + v.pub + '"]').append(writing_also_block);  
+          } else {
+              $('div.row#writing').find('ul[data-pub="' + v.pub + '"]').append(writing_block);
+          }
+
+      });
+
+
+      $.each(code, function(k,v) {
+          
+          var code_block = ich.code_block(v);
+          $('div.row#code').append(code_block);
+
+      });
+
+      $.each(design, function(k,v) {
+          
+          var design_block = ich.design_block(v);
+          var design_url_block = ich.design_url_block(v);
+
+          if (v.url !== '') {
+              $('div.row#design').append(design_url_block);  
+          } else {
+              $('div.row#design').append(design_block);
+          }
+
+      });
+
+      $.each(photo, function(k,v) {
+          
+          var photo_block = ich.photo_block(v);
+          $('div.row#photo').append(photo_block);
+
+      });
+
+
+
+    }
+
+    function eventListeners() {
+      $('ul.sticky a').on('click', function(e) {
+        e.preventDefault();
+        var id = $(this).attr('href').replace('#','');
+        var destination = $('div.row#' + id)
+
+        if ($(window).innerWidth() < 600) {
+          console.log($(window).innerWidth)
+          $('html,body').animate({scrollTop: $(destination).offset().top - 50});
+        } else {
+          $('html,body').animate({scrollTop: $(destination).offset().top});
+        }
+
+        
+        
+      });
+
+      $('div.block.code, div.block.design').on('mouseover', function() {
+          $(this).find('img').addClass('active')
+          $(this).find('p,div.mask').show();
+      }).on('mouseout', function() {
+          $(this).find('img').removeClass('active')
+          $(this).find('p,div.mask').hide();
+      });
+    }
+
+
+init();
